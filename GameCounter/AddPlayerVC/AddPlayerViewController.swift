83e8b03ebb8d97fd97870081131c4e5db802d53b
Ivan <waizord@yandex.ru>
2021-08-30 18:12:05 +0300

    Add: fix navBar on addVC

--- GameCounter/AddPlayerVC/AddPlayerViewController.swift
+++ GameCounter/AddPlayerVC/AddPlayerViewController.swift
@@ -12,6 +12,36 @@ class AddPlayerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Constants.colors.appBackgroundBlack
+        settingNavBar()
     }
 
+}
+//MARK: - Actions
+extension AddPlayerViewController {
+    @objc func tapBackButton() {
+        print("tap on Back")
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func tapAddButton() {
+        print("tap on Add")
+        self.navigationController?.popViewController(animated: true)
+    }
+}
+
+//MARK: - Setting NavBar
+extension AddPlayerViewController {
+    func settingNavBar() {
+        let backButton = UIBarButtonItem()
+        backButton.title = "Back"
+        backButton.target = self
+        backButton.action = #selector(tapBackButton)
+        self.navigationItem.leftBarButtonItem = backButton
+        
+        let addButton = UIBarButtonItem()
+        addButton.title = "Add"
+        addButton.target = self
+        addButton.action = #selector(tapAddButton)
+        self.navigationItem.rightBarButtonItem = addButton
+    }
 }
